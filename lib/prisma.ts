68c5795af4ba9aeb421c4070940e69947a0a2009
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-12-03 01:13:02 +0530

    Set up Prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,16 @@
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+// Learn more: https://pris.ly/d/help/next-js-best-practices
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
