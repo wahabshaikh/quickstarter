adbf882f2b1c2213a7e8f4011abc175c49680548
Wahab Shaikh <wahab.shaikh@somaiya.edu>
2021-12-16 04:18:02 +0530

    Upgrade Tailwind CSS to v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
 
       <main className="min-h-screen grid place-content-center place-items-center">
         <h1 className="text-5xl font-black">Welcome to QuickStarter!</h1>
-        <p className="text-xl font-semibold mt-4">
+        <p className="text-xl mt-2">
           A Next.js + Tailwind CSS + TypeScript + Prisma boilerplate.
         </p>
       </main>
